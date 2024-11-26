8c961f69efd8d738deb43b91ffb979976aa9674d
Henry <141684228+henry2craftman@users.noreply.github.com>
2024-11-26 18:35:48 +0900

    FIX: MPS Scene 추가 및 수정

---
+++ Assets/Scripts/MPS/Cylinder.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 공압이 공급되면 실린더 로드가 일정 거리만큼, 일정 속도로 전진 또는 후진한다.
+/// 전진 또는 후진 시, 전후진 Limit Switch(LS)가 작동한다.
+/// 속성: 실린더로드, Min-Max Range, Duration, 전후방 Limit Switch
+/// </summary>
+public class Cylinder : MonoBehaviour
+{
+    [SerializeField] Transform cylinderRod;
+    [SerializeField] float maxRange;
+    [SerializeField] float minRange;
+    [SerializeField] float duration;
+    [SerializeField] Transform forwardLS;
+    [SerializeField] Transform backwardLS;
+    [SerializeField] bool isForward = false;
+    [SerializeField] bool isRodMoving = false;
+
+    public void OnForwardBtnClkEvent()
+    {
+        if (isForward || isRodMoving) return;
+
+        StartCoroutine(MoveCylinder(cylinderRod, minRange, maxRange, duration));
+    }
+
+    public void OnBackwardBtnClkEvent()
+    {
+        if (!isForward || isRodMoving) return;
+
+        StartCoroutine(MoveCylinder(cylinderRod, maxRange, minRange, duration));
+    }
+
+    IEnumerator MoveCylinder(Transform rod, float min, float max, float duration)
+    {
+        isRodMoving = true;
+
+        Vector3 minPos = new Vector3(rod.transform.localPosition.x, min, rod.transform.localPosition.z);
+        Vector3 maxPos = new Vector3(rod.transform.localPosition.x, max, rod.transform.localPosition.z);
+
+        float currentTime = 0;
+
+        while (currentTime <= duration)
+        {
+            currentTime += Time.deltaTime;
+
+            rod.localPosition = Vector3.Lerp(minPos, maxPos, currentTime / duration);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        isRodMoving = false;
+        isForward = !isForward;
+    }
+
+}
---
+++ Assets/Scripts/MPS/MPSManager.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 모든 설비의 상태를 확인, 물체를 생성
+/// 속성: 각 설비들, 물체프리팹 변수
+/// </summary>
+public class MPSManager : MonoBehaviour
+{
+    [SerializeField] List<Cylinder> cylinders = new List<Cylinder>();
+    //[SerializeField] List<S> cylinders = new List<Cylinder>();
+    [SerializeField] List<MeshRenderer> lamps = new List<MeshRenderer>();
+    [SerializeField] Transform pusher;
+    [SerializeField] GameObject[] objPrefabs;
+    [SerializeField] Transform spawnPos;
+    int count;
+    Color redLamp;
+    Color yellowLamp;
+    Color greenLamp;
+
+    private void Start()
+    {
+        redLamp = lamps[0].material.GetColor("_BaseColor");
+        yellowLamp = lamps[1].material.GetColor("_BaseColor");
+        greenLamp = lamps[2].material.GetColor("_BaseColor");
+    }
+    public void OnSpawnObjBtnClkEvent()
+    {
+        if (count > objPrefabs.Length - 1) count = 0;
+
+        Instantiate(objPrefabs[count++], spawnPos.position, Quaternion.identity, transform);
+        //obj.transform.position = spawnPos.position;
+    }
+    public void OnLampOnOffBtnClkEvent(string name)
+    {
+        Color color;
+
+        switch (name)
+        {
+            case "Red":
+                color = lamps[0].material.GetColor("_BaseColor");
+                print(color);
+
+                if(color == redLamp)
+                {
+                    lamps[0].material.SetColor("_BaseColor", Color.black);
+                }
+                else if(color == Color.black)
+                {
+                    lamps[0].material.SetColor("_BaseColor", redLamp);
+                }
+                break;
+
+            case "Yellow":
+                color = lamps[1].material.GetColor("_BaseColor");
+                print(color);
+
+                if (color == yellowLamp)
+                {
+                    lamps[1].material.SetColor("_BaseColor", Color.black);
+                }
+                else if (color == Color.black)
+                {
+                    lamps[1].material.SetColor("_BaseColor", yellowLamp);
+                }
+                break;
+
+            case "Green":
+                color = lamps[2].material.GetColor("_BaseColor");
+                print(color);
+
+                if (color == greenLamp)
+                {
+                    lamps[2].material.SetColor("_BaseColor", Color.black);
+                }
+                else if (color == Color.black)
+                {
+                    lamps[2].material.SetColor("_BaseColor", greenLamp);
+                }
+                break;
+        }
+    }
+
+    public void OnConveyorBtnClkEvent()
+    {
+
+    }
+}
